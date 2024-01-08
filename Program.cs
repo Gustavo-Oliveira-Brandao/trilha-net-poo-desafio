4e162722e4a0f8366e95c7f96c43e5e83b9b76bc
Gustavo-Oliveira-Brandao <gustavooliveirabrandao2021@gmail.com>
2024-01-07 22:22:53 -0300

    Desafio Concluidogit statusgit statusgit status

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado!!!
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "40028922", modelo: "Nokia 12", imei: "11111111", memoria:128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Discord");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "89224002", modelo: "Iphone 15", imei: "22222222", memoria: 252);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Microsoft Teams");
